4556f4416e11f29df57d9e07f7cdee8295ed81b3
Burt Harris <Burt.Harris_github@azxs.33mail.com>
2017-07-03 12:29:40 -0700

    chore: adopt & enforce standardjs style.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const through = require('through2'); // through2 is a thin wrapperof  transform streams
-
-// Consts
-const PLUGIN_NAME = 'gulp-first';
+const through = require('through2') // through2 is a thin wrapperof  transform streams
 
 // Plugin level function(dealing with files or any arbitrary object)
-function first() {
-  return through.obj(function(file, enc, cb) {
-      this.end();
-      cb(null, file);
-  });
+function first () {
+  return through.obj(function (file, enc, cb) {
+    this.end()
+    cb(null, file)
+  })
 }
 
 // Exporting the plugin main function
-module.exports = first;
+module.exports = first
